a8b2dea750eab9e1aeb1c117b625e1fbc4a8c8c8
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2023-10-15 14:06:09 +0530

    fixed responsove range

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -23,19 +23,19 @@ function Navbar() {
       </div>
 
       <div className={`${openNav ? '' : 'hidden'} absolute top-0 left-0 w-full py-14 border-b border-seconday bg-white
-        | tab:block tab:static tab:py-0 tab:border-none tab:ml-auto tab:w-auto`}>
-        <ul className="text-primary flex flex-col text-center gap-5 tab:flex-row">
+        | tabM:block tabM:static tabM:py-0 tabM:border-none tabM:ml-auto tabM:w-auto`}>
+        <ul className="text-primary flex flex-col text-center gap-5 tabM:flex-row">
           {NavList.map((nav) => (
             <li>
               <a href={nav.link} className="hover:text-primary ease-in duration-200">{nav.name}</a>
             </li>
           ))}
         </ul>
-        <HiX className="absolute top-[1rem] right-4 text-xl cursor-pointer | tab:hidden" onClick={()=>setOpenNav(false)}/>
+        <HiX className="absolute top-[1rem] right-4 text-xl cursor-pointer | tabM:hidden" onClick={()=>setOpenNav(false)}/>
       </div> 
       <div className="flex gap-5 items-center">
         <HiSun className="ml-4 text-xl"/>
-        <HiMenuAlt1 className="text-xl tab:hidden" onClick={()=>setOpenNav(true)}/> 
+        <HiMenuAlt1 className="text-xl tabM:hidden" onClick={()=>setOpenNav(true)}/> 
       </div>
     </nav>
   );
